4cb5f049bee6928ff45ff5c5229c01fed264c321
Rebecca Leff <rebeccaleff@Rebeccas-MacBook-Pro.local>
2018-07-17 17:07:45 -0400

    Set up middleware to connect to db

---
+++ server/models/database.js
@@ -0,0 +1,24 @@
+const options = {
+  error(error, e) {
+    if (e.cn) {
+      // A connection-related error;
+      console.log("CN:", e.cn);
+      console.log("EVENT:", error.message);
+    }
+  }
+};
+
+let pgp = require('pg-promise')(options);
+let db = pgp('postgres://nqnxdhws:pmU5A841ugjh1uqwBCjckXQjagzVotF1@stampy.db.elephantsql.com:5432/nqnxdhws');
+
+db.connect()
+  .then(obj => {
+    console.log('Connected to database');
+    db.conn = obj;
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+  console.log('ERROR:', error.message || error);
+});
+
+module.exports = db;
